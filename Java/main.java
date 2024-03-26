547c3601928c43b805e5adf646726a10f932c8d9
CatfishGG <151020503+CatfishGG@users.noreply.github.com>
2024-03-26 10:42:56 +0000

    Discord4J

--- Java/main.java
+++ Java/main.java
@@ -4,7 +4,7 @@ import discord4j.core.event.domain.message.MessageCreateEvent;
 
 public class MyDiscordBot {
     public static void main(String[] args) {
-        final String token = "MTE5OTQzMjI4NjQ0NjI5NzI4MA.GJ4u_h.ym92xAxydTb9tOLfzDm5fYAULGPTaPC62pOJyk"; 
+        final String token = ""; 
         final DiscordClient client = DiscordClient.create(token);
         final GatewayDiscordClient gateway = client.login().block();
 
